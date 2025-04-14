8a9fd3c156028cfebab131e5bc6f3a1fa9278442
denialtorres <denial.torres@gmail.com>
2025-04-14 13:07:33 -0600

    Add bcrypt and enable has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates :password_digest, presence: true
+
+  has_secure_password
 end

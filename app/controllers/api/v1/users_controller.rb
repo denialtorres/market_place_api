83a813d235180e7ac9243dfc73001719cac64600
denialtorres <denial.torres@gmail.com>
2025-04-14 14:40:24 -0600

    Add CRUD actions for Users API endpoint
    The new actions allow creating, updating and deleting users through the API,
    along with comprehensive test coverage. Also adds pry for debugging.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,45 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :update, :destroy ]
 
   # GET /users/:id
   def show
     user = User.find(params[:id])
     render json: user
+  end
+
+  # POST /users
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /users/:id
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -13,4 +13,43 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(response.body)
     assert_equal @user.email, json_response["email"]
   end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post api_v1_users_url, params: { user: { email: "new_user@example.com", password: "password" } }, as: :json
+    end
+
+    assert_response :created
+    json_response = JSON.parse(response.body)
+    assert_equal "new_user@example.com", json_response["email"]
+  end
+
+  test "should not create user with taken email" do
+    assert_no_difference("User.count") do
+      post api_v1_users_url, params: { user: { email: @user.email, password: "password" } }, as: :json
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: "updated_email@example.com", password: "123456" } }, as: :json
+    assert_response :ok
+
+    json_response = JSON.parse(response.body)
+    assert_equal "updated_email@example.com", json_response["email"]
+  end
+
+  test "should not update user when invalid params are sent" do
+    patch api_v1_user_url(@user), params: { user: { email: "invalid_email", password: "123456" } }, as: :json
+    assert_response :unprocessable_entity
+  end
+
+  test "should destroy user" do
+    assert_difference("User.count", -1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+
+    assert_response :no_content
+  end
 end

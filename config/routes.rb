41d40ed2dd4041e2ae03be20d91590d29b4aa471
denialtorres <denial.torres@gmail.com>
2025-04-14 11:48:44 -0600

    Add API namespace with JSON defaults

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # Api definition
+
+  namespace :api, defaults: { format: :json } do
+    # we are going to list our resources here
+  end
+
+
+
+  # Render dynamic PWA files from app/views/pw
+  # a/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 

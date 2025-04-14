97f56e3f60677f16355d4203212f4501d45e13af
denialtorres <denial.torres@gmail.com>
2025-04-14 14:07:50 -0600

    Add user profile API endpoint
    Shows users endpoint added to API v1 namespace with tests and basic
    controller implementation.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      # we are going to list our resources here
+      resources :users, only: [:show]
     end
   end
 

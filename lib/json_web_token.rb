1a3aac78e310584f1bcc074e5219f21dd0343255
denialtorres <denial.torres@gmail.com>
2025-04-14 19:20:09 -0600

    include JWT gem for autenthicate users, logic for decode and encode tokents

---
+++ lib/json_web_token.rb
@@ -0,0 +1,14 @@
+# this class will encode and decode JWT tokens
+class JsonWebToken
+  SECRET_KEY = Rails.application.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SECRET_KEY).first
+    HashWithIndifferentAccess.new decoded
+  end
+end

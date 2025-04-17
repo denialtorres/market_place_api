e1ba2fa190ba6c143d157892337f31c7c5f3a1d1
denialtorres <denial.torres@gmail.com>
2025-04-17 09:32:34 -0600

    Add product quantity decrease upon placement creation

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,10 @@
 class Placement < ApplicationRecord
   belongs_to :order
   belongs_to :product, inverse_of: :placements
+
+  after_create :decrease_product_quantity!
+
+  def decrease_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end

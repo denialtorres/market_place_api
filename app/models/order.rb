69fab180abe9f575f4a331bfd97f162a2c1dc5a5
denialtorres <denial.torres@gmail.com>
2025-04-17 09:17:47 -0600

    Add product quantity tracking to order management

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,16 @@ class Order < ApplicationRecord
 
   before_validation :set_total!
 
+  # @param product_ids [Array<Hash>] something like this
+  # `[{product_id: 1, quantity: 2}, {product_id: 2, quantity: 3}]
+  # @yield [Placement] placements build
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      placement = placements.build(product_id: product_id_and_quantity[:product_id])
+      yield placement if block_given?
+    end
+  end
+
   private
 
   def set_total!

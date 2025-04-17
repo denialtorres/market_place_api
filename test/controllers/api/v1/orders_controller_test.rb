4f8a86d449a843eeae6267510142b1fd4958808a
denialtorres <denial.torres@gmail.com>
2025-04-16 23:04:32 -0600

    Add order show endpoint with product inclusion

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -19,4 +19,16 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(response.body)
     assert_equal @order.user.orders.count, json_response["data"].length
   end
+
+  test "should show order details" do
+    get api_v1_order_url(@order),
+    headers: { "Authorization" => JsonWebToken.encode(user_id: @order.user.id) },
+    as: :json
+
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_equal @order.id.to_s, json_response["data"]["id"]
+    include_product_attr = json_response["included"][0]["attributes"]
+    assert_equal @order.products.first.title, include_product_attr["title"]
+  end
 end

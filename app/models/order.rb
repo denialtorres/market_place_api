f21bb4168420df331bfd0f733b18229726b4f11a
denialtorres <denial.torres@gmail.com>
2025-04-17 09:43:25 -0600

    Add order validation for product quantity availability

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
+
+  validates_with EnoughProductsValidator
+
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   belongs_to :user

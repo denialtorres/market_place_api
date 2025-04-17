eea9e3aacafef596d3d2680186c95c3a218af732
denialtorres <denial.torres@gmail.com>
2025-04-17 11:05:00 -0600

    Add caching to serializers with 1 hour expiration

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
+
+  cache_options store: Rails.cache, namespace: "jsonapi-serializer", expires_in: 1.hour
+
   attributes :email
   has_many :products
 end
